46e3fdaa44fed146858b946ea2ecc08f0eebef9e
Rubén Cabrera <dev@rubencabrera.es>
2020-11-21 00:47:06 +0000

    feat: create conf from variables

--- source/entrypoint.sh
+++ source/entrypoint.sh
@@ -7,21 +7,36 @@ if [[ -z "${USUARIO}" ]]; then
 elif [[ -z "${PASSWORD}" ]]; then
   echo "PASSWORD no definida"
   exit 1
+elif [[ -z "${CUENTA}" ]]; then
+  echo "CUENTA no definida"
+  exit 1
+elif [[ -z "${DOMINIO}" ]]; then
+  echo "DOMINIO no definido"
+  exit 1
+elif [[ -z "${ZONAS}" ]]; then
+  echo "ZONAS no definidas"
+  exit 1
 fi
+
+: COMPROBAR_CADA=${COMPROBAR_CADA:-10}
+
+# $ZONAS es una variable con las zonas separadas por comas
+INPUTS=$(echo -n "${ZONAS}" | jq -Rsc 'split(",")')
+jq -Rn --arg cuenta "${CUENTA}" --argjson comprobar_cada "${COMPROBAR_CADA:=10}" --arg dominio "${DOMINIO}" --argjson inputs "${INPUTS}" ' $inputs | {$cuenta, $comprobar_cada, zonas: {($dominio): .} }' > /etc/dinaip.conf
 
 alias dina='dinaip -u "${USUARIO}" -p "${PASSWORD}"'
 # dinaip -u "${USUARIO}" -p "${PASSWORD}" -a rubencabrera.es:www
 # echo "-i"
-# dina -i
+dina -i
 # echo "Añadiendo zona www"
-# dina
-echo "-l Lista los dominios de esta cuenta"
-dina -l
-sleep 5
-echo "-s Muestra el estado"
-dina -s
+# echo "-l Lista los dominios de esta cuenta"
+# dina -l
+# sleep 5
+# echo "-s Muestra el estado"
+# dina -s
 # Para meter otra zona este es el comando.
 # TODO: ejecutar opcionalmente si le pasamos los argumentos necesarios
 # echo "Vamos a meter otra zona NUEVA"
 # dina -a rubencabrera.art:prueba
-echo "Fin del script, hasta luego"
+# echo "Fin del script, hasta luego"
+sleep 300
